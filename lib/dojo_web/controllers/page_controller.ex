8b80d48bfe0d8159e3a3a0e60f030d6f07b1be07
by34 <95256045+by34@users.noreply.github.com>
2022-12-13 16:58:27 -0800

    clean up

--- lib/dojo_web/controllers/page_controller.ex
+++ lib/dojo_web/controllers/page_controller.ex
@@ -9,10 +9,6 @@ defmodule DojoWeb.PageController do
     render(conn, "room.html", layout: {DojoWeb.LayoutView, "app.html"})
   end
 
-  def setup_ai(conn, _params) do
-    render(conn, "room.html", layout: {DojoWeb.LayoutView, "app.html"})
-  end
-
   # see: github.com/dwyl/ping
   @spec ping(Plug.Conn.t(), any) :: Plug.Conn.t()
   def ping(conn, params) do
