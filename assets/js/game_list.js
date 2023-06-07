8a384b0bb95d1af64b200e6c3d7dc6eacc9b6123
by34 <95256045+by34@users.noreply.github.com>
2023-06-07 13:34:08 -0700

    remove all css from home.html, GameList js functions

--- assets/js/game_list.js
+++ assets/js/game_list.js
@@ -2,12 +2,21 @@ class GameList {
     constructor (element) {
         this.element = element;
         this.id = element.id;
+        this.element.querySelector('.user_game').onclick = this.user_game_onclick.bind(this);
+    }
 
-        element.querySelector('.user_game').onclick = this.user_game_onclick;
+    hide_user_game() {
+        this.element.querySelector('.user_game').style.display = 'none';
+    }
+
+    add_game(game) {
+        this.element.querySelector('tbody').innerHTML += "<tr class=\"game\"><td>"+ game.player +"</td><td></td><td>"+ game.time +"</td></tr>"
     }
 
     user_game_onclick() {
-        alert('user_game_onclick');
+        var game = { player: "anon", time: "5+5" };
+        this.add_game(game);
+        // this.hide_user_game();
     }
 }
 export default GameList;
