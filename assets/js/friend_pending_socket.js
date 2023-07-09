9260e039b07bd63b6b16dad934d68db4125af0e2
by34 <95256045+by34@users.noreply.github.com>
2023-07-09 11:35:04 -0700

    remove unused js file and remove comment from friend pending socket js

--- assets/js/friend_pending_socket.js
+++ assets/js/friend_pending_socket.js
@@ -1,13 +1,4 @@
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "assets/js/app.js".
-
-// To use Phoenix channels, the first step is to import Socket,
-// and connect at the socket path in "lib/web/endpoint.ex".
-//
-// Pass the token on params as below. Or remove it
-// from the params if you are not using authentication.
 import {Socket} from "phoenix"
-import parseCookie from "./utils"
 
 let socket = new Socket("/room_socket", {params: {token: user_token}})
 socket.connect()
